d0b7d30ce9a1dd17e31b59948713aa2a15f8c6c7
Liv Olofsson <astrid_liv@hotmail.com>
2024-07-31 22:54:25 +0200

    added error-file and extraction of feature vectors from images with several ppl

--- migration.py
+++ migration.py
@@ -14,47 +14,47 @@ db_port = os.getenv("db_port")
 db_name = os.getenv("db_name")
 
 supabase = create_client(url, key)
-
-def upload_image_to_storage():
-    for _, _, filenames in os.walk("C:\\Users\\astri\\summer24\\FaceMatch\\test_bilder"):
-        for filename in filenames: 
-            if filename.endswith(".png") or filename.endswith(".jpg"):
-                image_path = str(os.path.join("test_bilder",filename))
-                with open(image_path, "rb") as image_file:
-                    image_name = os.path.basename(image_path)
-                    supabase.storage.from_("images").upload(file=image_file, path=image_name, file_options={"content-type": "image/png+jpeg"})
-
 DB_CONNECTION = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
 vx = vecs.create_client(DB_CONNECTION)
 images = vx.get_or_create_collection(name="images", dimension=128)
 
+def upload_image_to_storage():
+    error_file = open("errors.txt", "a")
+    for _, _, filenames in os.walk("C:\\Users\\astri\\summer24\\FaceMatch\\test_bilder"):
+        for filename in filenames: 
+            if filename.endswith(".png") or filename.endswith(".jpg"):
+                image_path = str(os.path.join("test_bilder",filename))
+                try:
+                    with open(image_path, "rb") as image_file:
+                        image_name = os.path.basename(image_path)
+                        supabase.storage.from_("images").upload(file=image_file, path=image_name, file_options={"content-type": "image/png+jpeg"})
+                except Exception as e:
+                    print(f"Failed to upload image {e}")
+                    error_file.write(f"{filename}, {e}\n")
+    error_file.close()
+
+
 def insert_image_feature_vector():
+    error_file = open("errors.txt", "a")
     for _, _, filenames in os.walk("C:\\Users\\astri\\summer24\\FaceMatch\\test_bilder"):
         for filename in filenames: 
             if filename.endswith(".png") or filename.endswith(".jpg"):
                 image_path = str(os.path.join("test_bilder",filename))
                 with open(image_path, "rb") as image_file:
                     url = f"https://thzrfpngunhhpdsgqiac.supabase.co/storage/v1/object/public/images/{filename}"
-                    represent = DeepFace.represent(image_path,model_name="Facenet", enforce_detection=False)
-                    dicti = represent[0]
-                    result = dicti.get("embedding")
+                    feature_vectors = DeepFace.represent(image_path,model_name="Facenet", enforce_detection=False)
+                    embeddings = [feature_vector["embedding"] for feature_vector in feature_vectors]
                     try:
-                        images.upsert(
-                            records=[
-                                (filename,
-                                result, 
-                                {"image_url": url}
-                                )
-                            ]
-                        )
+                        records = [(f"{filename}_{i}", embedding ,{"image_url": url}) for i, embedding in enumerate(embeddings)]
+                        images.upsert(records)
                         print("success")
                     except Exception as e:
-                        print(f"Failed to upsert records due to {e}")
-                    
-#upload_image_to_storage()
+                        print(f"Failed to upsert records {e}")
+                        error_file.write(f"{filename}, {e}\n")
+    error_file.close()
+             
+upload_image_to_storage()
 insert_image_feature_vector()
-
-index_response = images.create_index()
 
 try:
     images.create_index()
@@ -62,7 +62,4 @@ try:
 except Exception as e:
     print("Failed to create index.")
 
-
 vx.disconnect()
-
-print(os.name)
